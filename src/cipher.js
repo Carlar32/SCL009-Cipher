68f8da3bea3f6332888775625112d66e5a83e969
Carlar32 <carla.r.rivas2@gmail.com>
2019-04-04 13:23:58 -0300

    first commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,25 @@
-window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-    
-  },
-  decode: () => {
-    /* Acá va tu código */
-  }
-};
+// window.cipher = {
+//   encode: () => {
+ console.log(texto1,offset)
+ let texto2= "";
+ for (i = 0; i <=texto1.length; i++){
+  let ascii= texto1[i].charCodeAt();
+  console.log(texto1[i]);
+  
+}
+// //Convierte a código ASCII
+// let letra = "";
+// console.log(texto1[i].charCodeAt());
+
+  
+
+  //   }
+  // };
+  
+
+  
+  
+//   decode: () => {
+//     /* Acá va tu código */
+//   }
+// };
---
+++ src/index.js
@@ -0,0 +1,15 @@
+//Acá se guarda mensaje ingresado por usuario
+    let cifrar = document.getElementById("cifrar");
+    cifrar.addEventListener("click",()=> {
+        //Se guarda mensaje e imprime en consola
+        let texto1 = document.getElementById("texto1").value;
+        console.log(texto1);
+        //Recorriendo mensaje
+        
+
+        let numero = document.getElementById("numero").value;
+        console.log(numero);
+
+        
+        
+        });

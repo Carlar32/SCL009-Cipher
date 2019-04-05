2aecd59b3c623dbe1c3d2e672178a3c9ea2e269f
Carlar32 <carla.r.rivas2@gmail.com>
2019-04-05 13:01:47 -0300

    Se logra codificar y decodificar mas css

--- src/cipher.js
+++ src/cipher.js
@@ -1,25 +1,43 @@
-// window.cipher = {
-//   encode: () => {
- console.log(texto1,offset)
- let texto2= "";
- for (i = 0; i <=texto1.length; i++){
-  let ascii= texto1[i].charCodeAt();
-  console.log(texto1[i]);
-  
-}
-// //Convierte a código ASCII
-// let letra = "";
-// console.log(texto1[i].charCodeAt());
+window.cipher = {
+  encode: (texto1, numeroIngresado) => {
+    /* Acá va tu código */
+      //Recorriendo mensaje
+      let cifrado="";
+      for (let i=0; i < texto1.length; i++){
+        console.log(texto1[i]);
+        //Variable para guardar ASCII
+        let letra = texto1[i].charCodeAt();
+        console.log(letra);
+        //Variable para guardar ASCII con fórmula
+        let nuevoCodigo = (letra-65+numeroIngresado)%26+65;
+        console.log(nuevoCodigo);
+        //Variable para convertir ASCII a letra
+        let asciiLetra = String.fromCharCode(nuevoCodigo);
+        console.log(asciiLetra);
+        cifrado = cifrado += asciiLetra;
+        console.log(cifrado);}
+        //Return, resulatado FINAAAAL 
+        return cifrado;
 
-  
+},
+  decode: (texto1, numeroIngresado) => {
+    /* Acá va tu código */
+    let descifrado="";
+    for (let i=0; i < texto1.length; i++){
+      console.log(texto1[i]);
+      //Variable para guardar ASCII
+      let letra = texto1[i].charCodeAt();
+      console.log(letra);
+      //Variable para guardar ASCII con fórmula
+      let nuevoCodigo = (letra+65-numeroIngresado)%26+65;
+      console.log(nuevoCodigo);
+      //Variable para convertir ASCII a letra
+      let asciiLetra = String.fromCharCode(nuevoCodigo);
+      console.log(asciiLetra);
+      descifrado = descifrado += asciiLetra;
+      console.log(descifrado);}
+      //Return, resulatado FINAAAAL 
+      return descifrado;
+  }
+};
 
-  //   }
-  // };
-  
-
-  
-  
-//   decode: () => {
-//     /* Acá va tu código */
-//   }
-// };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
 //Acá se guarda mensaje ingresado por usuario
-    let cifrar = document.getElementById("cifrar");
-    cifrar.addEventListener("click",()=> {
-        //Se guarda mensaje e imprime en consola
-        let texto1 = document.getElementById("texto1").value;
-        console.log(texto1);
-        //Recorriendo mensaje
-        
+let botoncifrar = document.getElementById("cifrar");
+botoncifrar.addEventListener("click",()=> {
+    //Se guarda mensaje e imprime en consola
+    let texto1 = document.getElementById("texto1").value;
+    // console.log(texto1)
+    //Variable cambia de string a número (parseInt)
+    let numeroIngresado = parseInt(document.getElementById("numero").value);
+    //Muestra en resultado de la función encode en caja2
+    document.getElementById("texto2").value = window.cipher.encode (texto1, numeroIngresado);
+   // console.log(texto2);
 
-        let numero = document.getElementById("numero").value;
-        console.log(numero);
+    }
+);
+let botondescifrar = document.getElementById("descifrar");
+botondescifrar.addEventListener("click",()=> {
+    //Se guarda mensaje e imprime en consola
+    let texto1 = document.getElementById("texto1").value;
+    // console.log(texto1)
+    //Variable cambia de string a número (parseInt)
+    let numeroIngresado = parseInt(document.getElementById("numero").value);
+    //Muestra en resultado de la función encode en caja2
+    document.getElementById("texto2").value = window.cipher.decode (texto1, numeroIngresado);
+   // console.log(texto2);
 
-        
-        
-        });
+    }
+);
+
+let botonLimpiar = document.getElementById("limpiar");
+botonLimpiar.addEventListener("click",()=> {
+    document.getElementById("numero").value = "";  
+    document.getElementById("texto1").value = "";  
+    document.getElementById("texto2").value = "";   
+
+}
+);
